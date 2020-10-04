522adc6a5acf451725bc589045e63e19055b72b7
Alexander Misel <7542439+AlexanderMisel@users.noreply.github.com>
2020-10-04 13:32:53 +0800

    Update utils.lua

--- utils.lua
+++ utils.lua
@@ -100,4 +100,21 @@ function M.urlEncode(str)
   return str
 end
 
+function M.utf8to32(utf8str)
+  local seq, val = 0, nil
+  for i = 1, #utf8str do
+    local c = string.byte(utf8str, i)
+    if seq == 0 then
+      seq = c < 0x80 and 1 or c < 0xE0 and 2 or c < 0xF0 and 3 or
+            c < 0xF8 and 4 or c < 0xFC and 5 or c < 0xFE and 6 or
+          error("invalid UTF-8 character sequence")
+      val = bit.band(c, 2^(8-seq) - 1)
+    else
+      val = bit.bor(bit.lshift(val, 6), bit.band(c, 0x3F))
+    end
+    seq = seq - 1
+  end
+  return val
+end
+
 return M
